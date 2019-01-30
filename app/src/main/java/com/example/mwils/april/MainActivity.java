bf9191978cef410cb1b15f6cb10a693fc1656004
Matthew Wilson <mwilson1310@gmail.com>
2019-01-30 13:03:26 +0000

    Added a Logout button that returns the User to the login page from the Main page.

--- app/src/main/java/com/example/mwils/april/MainActivity.java
+++ app/src/main/java/com/example/mwils/april/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.mwils.april;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Creating instance of Firebase authenticator
+    private FirebaseAuth auth;
+
+    //assigns a variable to the logout button
+    //Creating variable for the LogOut button
+    private Button logout = (Button)findViewById(R.id.btnLogOut);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //creates an instance of the main class
+        auth = FirebaseAuth.getInstance();
+
+        //loging the user out when the button is clicked
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //logs the user out of the application
+                auth.signOut();
+                finish();
+                //redirects the user back to the login page
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
+            }//end onClick method
+        });//end onClickListender
+    }//end onCreate method
+}//end class
