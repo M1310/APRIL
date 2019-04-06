dc43bc368f8ede5f387b1c003d35b6cfdc42f52c
Matthew Wilson <mwilson1310@gmail.com>
2019-04-06 14:46:39 +0100

    Added Nutrition Information Page with 4 tabs for each section.

--- app/src/main/java/com/example/mwils/april/Nutrition.java
+++ app/src/main/java/com/example/mwils/april/Nutrition.java
@@ -1,44 +1,42 @@
 package com.example.mwils.april;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Nutrition extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    //Creating variables to hold tie to the buttons
+    private Button info; Button illness; Button meals; Button smoothies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        //assigning the info variable to btnNutrInfo
+        info = (Button) findViewById(R.id.btnNutrInfo);
+        //assigning the illness variable to btnNutrIllness
+        illness = (Button) findViewById(R.id.btnNutrIllness);
+        //assigning the meals variable to btnNutrMeals
+        meals = (Button) findViewById(R.id.btnNutrMeals);
+        //assigning the smoothies variable to btnNutrSmoothies
+        smoothies = (Button) findViewById(R.id.btnNutrSmoothies);
+
+        //navigating the user to the Nutrition Main page
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Nutrition.this, NutritionInformation.class));
+            }//end onClick method
+        });//end OnClickListener
+    }//end onCreate method
 
 }
---
+++ app/src/main/java/com/example/mwils/april/SectionsPageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.mwils.april;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsPageAdapter extends FragmentPagerAdapter {
+
+    //Used to keep track of the Fragments
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    //Used to keep track of the name of the Fragments
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    //Adds the actual fragment and page title
+    public void addFragment(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }//end addFragment method
+
+    public SectionsPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }//returns fragment title
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }//returns fragment itself
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
